d0f28ee3d4e9e5b16e3d8aaa08657948afbaaf9f
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2025-10-06 21:30:50 +0700

    chỉnh device

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import json
 import argparse
 from trainer import train
 import copy
-import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 
 def main():
     args = setup_parser().parse_args()
