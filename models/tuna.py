e3345b237d7c54637a6ff1c749ec01cadfeb179b
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2025-10-25 12:01:44 +0700

    Update tuna.py

--- models/tuna.py
+++ models/tuna.py
@@ -434,6 +434,7 @@ class Learner(BaseLearner):
             adapter_probs = []
             adapter_distances = []
             for i in range(self._cur_task + 1): #cho chạy qua từng adapter để tính đặc trưng và xác suất, tính entropy và khoảng cách mahalanobis giữa đặc trưng của input task hiện tại với các lớp đã học
+
                 with torch.no_grad():
                     features = self._network.backbone(inputs, adapter_id=i, train=False)["features"]
                     logits = self._network.fc(features)["logits"][:, :self._total_classes] * self.args['scale']
@@ -444,9 +445,13 @@ class Learner(BaseLearner):
                 adapter_entropies.append(entropy) # cất trữ entropy của từng adapter đối với batch hiện tại
                 adapter_probs.append(probs) # cất trữ xác suất của từng adapter đối với batch hiện tại
                 adapter_distances.append(distances) # cất trữ khoảng cách mahalanobis của từng adapter đối với batch hiện tại
-            adapter_entropies = torch.stack(adapter_entropies)  # [num_adapters, bs]
-            adapter_distances = torch.stack(adapter_distances)  # [num_adapters, bs]
+            adapter_entropies = torch.stack(adapter_entropies)  # stack lại để được tensor [num_adapters, bs], trong đó bs là kích thước batch, num_adapters là số adapter đã học
+            adapter_distances = torch.stack(adapter_distances)  # 
             adapter_probs = torch.stack(adapter_probs)  # [num_adapters, bs, classes]
+            for idx, d in enumerate(adapter_distances):
+                if d is None:
+                    print(f"⚠️ Adapter {idx} trả về None từ _compute_mahalanobis_distance")
+
             distance_min, distance_max = adapter_distances.min(dim =0).values, adapter_distances.max(dim = 0).values
             entropy_min, entropy_max = adapter_entropies.min(dim=0).values, adapter_entropies.max(dim=0).values
             distance_range = (distance_max - distance_min).clamp_min(1e-12)
